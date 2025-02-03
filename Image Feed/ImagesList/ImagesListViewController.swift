7f51ffa426485d0a9041e43ce56ad89ff65d4a4c
Hajime4life <doguziagerman@gmail.com>
2025-02-03 16:25:02 +0300

    Добавил переход на нажатую картинку

--- Image Feed/ImagesList/ImagesListViewController.swift
+++ Image Feed/ImagesList/ImagesListViewController.swift
@@ -1,23 +1,52 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
+    
     // MARK: - Outlets
     @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var imageView: UIImageView!
 
+    // MARK: - Public Properties
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
     // MARK: - Private Properties
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         return formatter
     }()
+    private let currentDateTime = Date()
     
     // MARK: - Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    /// тут обработка сигвея
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                assertionFailure("Invalid segue destination")
+                return
+            }
+            
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -42,8 +71,9 @@ extension ImagesListViewController: UITableViewDataSource {
 
 // MARK: - Delegate
 extension ImagesListViewController: UITableViewDelegate {
+    // Тут переход на image view
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: - Добавить логику при нажатии на ячейку
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -68,10 +98,13 @@ extension ImagesListViewController {
         }
 
         cell.cellImage.image = image
-        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.dateLabel.text = dateFormatter.string(from: currentDateTime)
 
         let isLiked = indexPath.row % 2 == 0
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
+
+
+
